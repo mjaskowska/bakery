3e1c0bc9c1c08de13f1f2db1afb9550f2127c826
Magda <jaskowska.magda@gmail.com>
2020-12-20 14:52:54 +0000

    code cleanup

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ toggleMenu
   .to(lineMiddle, 0.125, { scaleY: 0 })
   .to(lineTop, 0.125, { rotation: 45, transformOrigin: "50% 50%", y: 9, ease: Power2.easeInOut }, "cross")
   .to(lineBottom, 0.125, { rotation: -45, transformOrigin: "50% 50%", y: -9, ease: Power2.easeInOut }, "cross");
- 
+
 hamburger.addEventListener('click', _ => {
   hamburger.classList.toggle('js-cross');
   toggleMenu.reversed() ? toggleMenu.play() : toggleMenu.reverse();
@@ -33,14 +33,14 @@ hamburger.addEventListener('click', _ => {
 // hamburger menu slide in/out
 const menuItems = document.querySelector('.menu-items')
 hamburger.addEventListener('click', _ => {
-menuItems.classList.toggle("menu-active")
-} )
+  menuItems.classList.toggle("menu-active")
+})
 
 
 // hero animation
 const heroContent = document.querySelector('.hero-list');
 
-gsap.fromTo(heroContent.children, {x: '+=150', opacity: 0}, {x: 0, opacity: 1, duration: 1.5, ease: 'easeInOut'})
+gsap.fromTo(heroContent.children, { x: '+=150', opacity: 0 }, { x: 0, opacity: 1, duration: 1.5, ease: 'easeInOut' })
 
 // scroll trigger all sections animation
 gsap.registerPlugin(ScrollTrigger);
@@ -48,15 +48,17 @@ gsap.registerPlugin(ScrollTrigger);
 const sections = document.querySelectorAll('section');
 
 sections.forEach(section => {
-  gsap.fromTo(section.children, {y: '+=50', opacity: 0}, {y: 0, opacity: 1, duration: 1, ease: 'easeInOut', scrollTrigger: {
-    trigger: section,
-    start: 'top 80%'
-  }})
+  gsap.fromTo(section.children, { y: '+=50', opacity: 0 }, {
+    y: 0, opacity: 1, duration: 1, ease: 'easeInOut', scrollTrigger: {
+      trigger: section,
+      start: 'top 80%'
+    }
+  })
 })
 
 const menuItemAll = document.querySelectorAll('.menu-item')
 menuItemAll.forEach(item => {
-  item.addEventListener('click', function() {
+  item.addEventListener('click', function () {
     menuItems.classList.remove("menu-active")
     toggleMenu.reverse()
   })
@@ -67,7 +69,7 @@ menuItemAll.forEach(item => {
 var slideNo = 1;
 Carousel(slideNo);
 
-function Carousel(n){
+function Carousel(n) {
   var slides = document.getElementsByClassName("image-container");
   var dots = document.getElementsByClassName("dot");
   if (n > slides.length) {
@@ -86,9 +88,9 @@ function Carousel(n){
   dots[slideNo - 1].className += " active";
 
 }
-function newSlide(n){
+function newSlide(n) {
   Carousel(slideNo += n);
 }
-function currentSlide(n){
+function currentSlide(n) {
   Carousel(slideNo = n);
 }
